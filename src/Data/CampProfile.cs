73bae1cfbf60867fdbbf38ec3e1f4be0c5fcd0a0
SamIge7 <sige@spartaglobal.com>
2020-04-15 10:38:36 +0100

    Commit #3 15/4/20: Building your first API

--- src/Data/CampProfile.cs
+++ src/Data/CampProfile.cs
@@ -11,7 +11,15 @@ namespace CoreCodeCamp.Data
     {
         public CampProfile()
         {
-            this.CreateMap<Camp, CampModel>();
+            this.CreateMap<Camp, CampModel>()
+                .ForMember(c => c.Venue, o => o.MapFrom(m => m.Location.VenueName))
+                .ForMember(c => c.AddressLine1, o => o.MapFrom(m => m.Location.Address1))
+                .ForMember(c => c.AddressLine2, o => o.MapFrom(m => m.Location.Address2))
+                .ForMember(c => c.AddressLine3, o => o.MapFrom(m => m.Location.Address3))
+                .ForMember(c => c.CityorTown, o => o.MapFrom(m => m.Location.CityTown))
+                .ForMember(c => c.StateorProvince, o => o.MapFrom(m => m.Location.StateProvince))
+                .ForMember(c => c.PostCode, o => o.MapFrom(m => m.Location.PostalCode))
+                .ForMember(c => c.Country, o => o.MapFrom(m => m.Location.Country));
         }
     }
 }
--- src/Models/CampModel.cs
+++ src/Models/CampModel.cs
@@ -12,5 +12,14 @@ namespace CoreCodeCamp.Models
         public string Moniker { get; set; }
         public DateTime EventDate { get; set; } = DateTime.MinValue;
         public int Length { get; set; } = 1;
+
+        public string Venue { get; set; }
+        public string AddressLine1 { get; set; }
+        public string AddressLine2 { get; set; }
+        public string AddressLine3 { get; set; }
+        public string CityorTown { get; set; }
+        public string StateorProvince { get; set; }
+        public string PostCode { get; set; }
+        public string Country { get; set; }
     }
 }
