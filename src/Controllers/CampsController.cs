538663edd906f34c40f8bc4e75dd0a75b2084506
SamIge7 <sige@spartaglobal.com>
2020-04-15 11:04:52 +0100

    Commit #5 15/4/20: Implementing Searching

--- src/Controllers/CampsController.cs
+++ src/Controllers/CampsController.cs
@@ -52,7 +52,23 @@ namespace CoreCodeCamp.Controllers
             }
             catch (Exception)
             {
+                return this.StatusCode(StatusCodes.Status500InternalServerError, "Database failure");
+            }
+        }
 
+        [HttpGet("search")]
+        public async Task<ActionResult<CampModel[]>>SearchByDate(DateTime theDate, bool includeTalks = false)
+        {
+            try
+            {
+                var results = await _repository.GetAllCampsByEventDate(theDate, includeTalks);
+
+                if (!results.Any()) return NotFound();
+
+                return _mapper.Map<CampModel[]>(results);
+            }
+            catch (Exception)
+            {
                 return this.StatusCode(StatusCodes.Status500InternalServerError, "Database failure");
             }
         }
